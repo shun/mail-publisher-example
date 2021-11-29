e4b88d78fdf8e7f555e19cdb9027c5d76ac0c275
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2021-11-30 00:48:06 +0900

    add to send mail

--- pages/api/sendmail.ts
+++ pages/api/sendmail.ts
@@ -1,10 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { renderToString } from "react-dom/server";
+import { createTransport } from "nodemailer";
+
 import { MailBody } from "./MailBody";
 
 type Data = {
   status: string
 }
+
+const transporter = createTransport({
+  host: "MAIL SERVER ADDRESS",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "ACCOUNT NAME",
+    pass: "PASSWORD",
+  }
+});
 
 export default function handler(
   req: NextApiRequest,
@@ -17,6 +29,17 @@ export default function handler(
     "orange",
   ];
   const body = renderToString(MailBody(fruits));
-  console.log("body", body);
-  res.status(200).json({ status: 'OK' })
+  try {
+    transporter.sendMail({
+      from: "hogehoge@example.com",
+      to: "hogehoge2@example.com",
+      subject: "mail example",
+      html: body,
+    });
+
+    res.status(200).json({ status: 'OK' })
+  } catch (error) {
+    console.error("fail to send mail.", error);
+    res.status(500).json({ status: 'NG' })
+  }
 }
